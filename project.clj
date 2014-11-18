22b3dbf6bc06ae102cb13602401b85604600aab4
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-18 09:48:53 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.spootnik/pickler "0.1.0"
+(defproject org.spootnik/pickler "0.1.1"
   :description "just enough pickle for graphite"
   :url "https://github.com/pyr/pickler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]])
